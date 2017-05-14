55150458aa54ed94aafcdb91bb94d8daa7321674
arkhabmaulana12 <arkhabmaulana12@gmail.com>
2017-05-14 12:23:13 +0700

    First Commit.

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl407/movie/adapter/SourceAdapter.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl407.movie.adapter;
+
+/**
+ * Created by ALAN on 14/05/2017.
+ */
+public class SourceAdapter {
+
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl407/movie/model/SourcesResponse.java
@@ -0,0 +1,7 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl407.movie.model;
+
+/**
+ * Created by ALAN on 14/05/2017.
+ */
+public class SourcesResponse {
+}
